758a923af4fc720838f066f5960ced4e12569e5c
ImGabreuw <imgabreuw.dev@gmail.com>
2024-11-09 16:17:44 -0300

    chore: add debug mode using valgrind

--- run.sh
+++ run.sh
@@ -10,6 +10,21 @@ CFLAGS="-Wall -Werror -I$INCLUDE_DIR -pthread"
 
 SRC_FILES=$(find $SRC_DIR -name "*.c" ! -path "$TEST_DIR/*")
 
+DEBUG=false
+
+# Parse command-line arguments
+while getopts "d" opt; do
+    case $opt in
+    d)
+        DEBUG=true
+        ;;
+    *)
+        echo "Usage: $0 [-d]"
+        exit 1
+        ;;
+    esac
+done
+
 echo "Compiling project..."
 
 gcc $CFLAGS -o $OUTPUT $SRC_FILES
@@ -18,11 +33,17 @@ if [ $? -eq 0 ]; then
     echo "Compilation successful! Output binary: $OUTPUT"
 else
     echo "Compilation failed!"
+    exit 1
 fi
 
-./$OUTPUT
+if [ "$DEBUG" = true ]; then
+    echo "Running in debug mode with valgrind..."
+    valgrind --leak-check=full ./$OUTPUT
+else
+    ./$OUTPUT
+fi
 
-# "$?" is aspecial variable in Bash that holds the exit status of the last command.
+# "$?" is a special variable in Bash that holds the exit status of the last command.
 if [ $? -eq 0 ]; then
     echo "Program executed successfully!"
 else
