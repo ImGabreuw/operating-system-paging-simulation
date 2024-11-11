7666871e8f84c5a19a1175a35ba608ea01c7fe22
L. Sarai <133360110+Lsarai23@users.noreply.github.com>
2024-11-11 10:39:55 -0300

    fix: frame validation

--- src/memory_management_unit.c
+++ src/memory_management_unit.c
@@ -7,13 +7,12 @@
 int translate_address(Process *process, int logicalAddress)
 {
     int pageNum = logicalAddress / FRAME_SIZE;
-    Page *page = get_page(process->logical_memory, pageNum);
-
-    if (page->is_loaded)
-        return -1;
+    //Page *page = get_page(process->logical_memory, pageNum);
 
     int offset = logicalAddress % FRAME_SIZE;
     int frameNum = get_frame_number(process->page_table, pageNum);
+
+    if(frameNum == -1) return frameNum;
 
     return frameNum * FRAME_SIZE + offset;
 }
